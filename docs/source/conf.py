e766ff89c3359a4c973d7928551d3a242cd8d62c
Philip Crotwell <crotwell@seis.sc.edu>
2025-06-18 23:00:14 -0400

    docs via sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'taup'
+copyright = '2025, Philip Crotwell'
+author = 'Philip Crotwell'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    "autodoc2",
+    'sphinx.ext.intersphinx',
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+autodoc2_packages = [
+    "../../src/taup",
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,37 @@
+.. taup documentation master file, created by
+   sphinx-quickstart on Wed Jun 18 22:47:28 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+TauP Python Interaction
+=======================
+
+
+Access The TauP Toolkit from Python.
+
+
+The latest source is at GitHub,
+`https://github.com/crotwell/taup_python <https://github.com/crotwell/taup_python>`_
+.
+
+.. code-block::
+
+  pip install taup
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   README <readme_link>
+   apidocs/index
+   Source <https://github.com/crotwell/taup_python>
+   PyPI <https://pypi.org/project/taup/>
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
