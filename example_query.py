5db8abee2ff7c08b865fd47211164cef3d08616f
Philip Crotwell <crotwell@seis.sc.edu>
2025-06-18 23:01:07 -0400

    example does calc via loop over event, stations

--- example_query.py
+++ example_query.py
@@ -1,18 +1,30 @@
 import taup
 import requests
 
-with taup.TauPServer("../TauP/build/install/TauP/bin/taup") as timeserver:
-    timeParams = taup.TimeQuery()
-    timeParams.phase = ["P", "S"]
-    timeParams.event = [35, -50]
-    timeParams.sid = ["CO_HAW", "CO_BIRD"]
-    timeParams.evdepth = [100]
-    timeParams.model = 'ak135'
+eventLatLons = [ [35, -50], [-29, 45]]
+staLatLons = [ [34, -80], [35, -81]]
 
-    jsonTimes = timeParams.calc(timeserver)
-    if len(jsonTimes["arrivals"]) == 0:
-        print("No arrivals...")
-    else:
-        print("Phase Depth    Dist  Time")
-    for a in jsonTimes["arrivals"]:
-        print(f"{a['phase']}   {a['sourcedepth']} {a['distdeg']} {a['time']}  {a['desc']}")
+with taup.TauPServer("../TauP/build/install/TauP/bin/taup") as timeserver:
+
+    timeParams = taup.TimeQuery()
+    # params that will stay the same
+    timeParams.phase = ["P", "S"]
+    timeParams.model = 'ak135'
+    timeParams.geodetic = True
+
+    # params that will vary
+    for event in eventLatLons:
+        timeParams.event = event
+        timeParams.evdepth = [100]
+        for sta in staLatLons:
+            timeParams.station = sta
+
+            jsonTimes = timeParams.calc(timeserver)
+            if len(jsonTimes["arrivals"]) == 0:
+                print(f"No arrivals...{event} to {sta}")
+            else:
+                print("Phase Depth    Dist  Time")
+            for a in jsonTimes["arrivals"]:
+                desc= a['desc'] if "desc" in a else ""
+                #print(a)
+                print(f"{a['phase']}   {a['sourcedepth']} {a['distdeg']} {a['time']}  {desc}")
