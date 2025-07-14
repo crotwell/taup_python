9865cc6aa4cdb33da89a51674f703e9a784a658c
Philip Crotwell <crotwell@seis.sc.edu>
2025-07-14 12:31:16 -0400

    ver in comment

--- src/taup/taupversion.py
+++ src/taup/taupversion.py
@@ -1,7 +1,7 @@
 
 # Version of TauP the python code corresponds to. 
 # Use with other versions may not work.
-TAUP_VERSION = "3.0.2-SNAPSHOT_2025-07-11T154351"
+TAUP_VERSION = "3.0.2-SNAPSHOT_2025-07-14T122228"
 
 TAUP_DOWNLOAD = "https://doi.org/10.5281/zenodo.15426279"
 
