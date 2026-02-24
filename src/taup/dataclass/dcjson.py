aeed6b00cf6840b9e04370b2b6c4098d0f396e28
Philip Crotwell <crotwell@seis.sc.edu>
2026-02-24 08:57:28 -0500

    remove null items from json output

--- src/taup/dataclass/dcjson.py
+++ src/taup/dataclass/dcjson.py
@@ -3,5 +3,6 @@ import dataclasses, json
 class DataClassJsonEncoder(json.JSONEncoder):
     def default(self, o):
         if dataclasses.is_dataclass(o):
-            return dataclasses.asdict(o)
+            d = dataclasses.asdict(o)
+            return {k:v for k,v in d.items() if v is not None}
         return super().default(o)
