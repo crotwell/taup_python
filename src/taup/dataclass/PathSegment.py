49ca555c6f49783f649991342e4da42f52d6eb29
Philip Crotwell <crotwell@seis.sc.edu>
2026-03-31 17:27:14 -0400

    add more props to pathsegment

--- src/taup/dataclass/PathSegment.py
+++ src/taup/dataclass/PathSegment.py
@@ -6,11 +6,16 @@ from .TimeDist import TimeDist
 class PathSegment:
     name: str
     wavetype: str
+    updown: str
+    prevendaction: str
+    endaction: str
     segment: list[TimeDist] = field(default_factory=list)
 
     @classmethod
     def from_json(cls, jsonObj):
-        ps = PathSegment(jsonObj['name'], jsonObj['wavetype'])
+        ps = PathSegment(jsonObj['name'], jsonObj['wavetype'],
+                         jsonObj['updown'],
+                         jsonObj['prevendaction'], jsonObj['endaction'] )
         for p in jsonObj['segment']:
             ps.segment.append(TimeDist.from_json(p))
         return ps
