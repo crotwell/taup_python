143b15e618c737319cf5247318648dfe49d38b6a
Philip Crotwell <crotwell@seis.sc.edu>
2026-09-11 17:06:04 -0400

    regen bindings from java

--- src/taup/taupversion.py
+++ src/taup/taupversion.py
@@ -1,7 +1,7 @@
 
 # Version of TauP the python code corresponds to. 
 # Use with other versions may not work.
-TAUP_VERSION = "3.2.0"
+TAUP_VERSION = "3.2.2-SNAPSHOT_2026-09-11T165623"
 
 TAUP_DOWNLOAD = "https://doi.org/10.5281/zenodo.10794857"
 
